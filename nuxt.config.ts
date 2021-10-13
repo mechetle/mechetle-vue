3ad12a87bc3c99d45e015723944dd434b47ee817
mechetle <pigsky123@gmail.com>
2021-10-13 16:13:29 +1100

    first commit lol

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+import { defineNuxtConfig } from 'nuxt3'
+
+export default defineNuxtConfig({
+    css: [
+        // Foundation:
+        // (use the sass version in the future and then customize it so it no brokey)
+        '~/assets/css/foundation.css',
+        // Mechetle's global styling shit:
+        '~/assets/css/stylesheet.scss'
+      ]
+
+})
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Test <span id="sass">sass</span></h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+h1 {
+    #sass {
+        color: aqua;
+    }
+}
+</style>
