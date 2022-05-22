1d165df5a94d289c5cc817dbeb7f06f22ead237f
mechetle <23493254+mechetle@users.noreply.github.com>
2022-05-22 17:59:31 +1000

    Fixed images

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@ import { defineNuxtConfig } from 'nuxt'
 
 export default defineNuxtConfig({
   css: [
-    // Foundation:
-    // (use the sass version in the future and then customize it so it no brokey)
-    '~/assets/css/foundation.css',
     // Mechetle's global styling shit:
     '~/assets/scss/main.scss'
   ],
---
+++ pages/test.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <h1>Test route</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return { 
+            rellaxPattern: [-0.1, 0.1, -0.1, 0.1]
+            //rellaxPattern: [-4, 2, -2, 4]
+        }
+    },
+
+    // this used to cause error 505, but may cause other issues
+    beforeMount() {
+    },
+
+    mounted() {
+        /* 
+         * homepage animations:
+         */
+        var rellax = new Rellax('.rellax', {
+            //center: true, causes issues
+            vertical: true,
+            horizontal: false
+        });
+
+        // Wrap every letter in MECHETLE in header
+        let textWrapper = document.querySelectorAll('#me-mechetle > .rellax:nth-child(n+3)');
+        textWrapper.forEach(element => {
+            element.innerHTML = element.textContent.replace(/\S/g, "<span class='header-letter'>$&</span>");
+        });
+        
+
+    }
+}
+</script>
+
+<script setup>   
+import HeaderFp from "../components/layout/header/header-fp.vue";
+import Cta from "../components/sections/cta.vue";
+import WorkThumb from "../components/cards-widgets/work-thumb.vue";
+import Container from "../components/layout/grid/container.vue";
+import GridX from "../components/layout/grid/grid-x.vue";
+import GridX1 from "../components/layout/grid/grid-x.vue";
+import VideoM from "../components/cards-widgets/video-m.vue";
+
+const {data: posts} = await useFetch("/data?limit=4")
+
+
+</script>
+
+<style lang="scss" scoped>
+    h1 {
+        #sass {
+            color: blue;
+        }
+    }
+</style>
