5ca7b72432092f19b0a823933167d21f9143b6de
Steven B-m Nguyen <23493254+mechetle@users.noreply.github.com>
2022-05-23 20:56:07 +1000

    Revert "fixed nuxt layout"
    This reverts commit 6e216af73645d7d0f7e160078c78a96c0ce4e140.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <NavMain/>
+    <NavPrimary/>
     <slot />
     <!-- <Beta /> -->
     <FooterM />
@@ -7,13 +7,13 @@
 
 <script>
 import FooterM from '../components/layout/footer/footer-m.vue'
-import NavMain from '../components/layout/navigation/nav-main.vue'
+import NavPrimary from '../components/layout/navigation/nav-primary.vue'
 import Beta from '../components/sections/beta.vue'
 
 export default {
     components: {
         FooterM,
-        NavMain,
+        NavPrimary,
         Beta
     }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,135 +1,135 @@
 <template>
-        <HeaderFp />
-    
-        <section id="who-am-i">
-            <Container class="fluid">
-                <GridX>
-                    <div class="cell medium-8 large-5" id="wai-wrapper">
-                        <h2>Who am I?</h2>
-                        <q>Hello I’m Steven B-M Nguyen, an enthusiastic and cheerful motion, web and graphic designer from
-                            Melbourne, Australia.</q>
-    
-                        <a href="/about-me" class="button">Read more</a>
-                    </div>
-    
-                    <div class="cell medium-4 large-5"> 
-                        <!-- <VideoM></VideoM> -->
-                    </div>
-    
-                </GridX>
-            </Container>
-        </section>
-    
-        <section id="featured-work">
-            <Container class="fluid extended">
-                <h2 class="rellax" data-rellax-speed="-0.8" data-rellax-percentage="1">Some of my work</h2>
-                <GridX class="grid-margin-x">
-                    <div class="cell medium-6 large-6 work-thumb rellax" data-rellax-speed="-1" data-rellax-percentage="0.5">
-                        <a href="/portfolio/case/nihdc4">
-                            <div class="work-thumb-wrapper">
-                                <div class="work-thumb-wrapper-cont">
-                                    <img src="/assets/img/thumbs/nihdc4.png" alt="NIHDC4 motion design entry">
-                                    <h3>NIHDC4</h3>
-                                    <p>Motion design entry</p>
-    
-                                    <div class="cat-wrapper">
-                                        <div class="categories">
-                                            <span>&#9866; &nbsp</span>
-                                            <span>Motion design</span>
-                                        </div>
-                                    </div>
-    
-                                </div>
-                                <div class="read-more-wrapper">
-                                    <div class="btn-read-more">See</div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="cell medium-6 large-6 work-thumb rellax" data-rellax-speed="1" data-rellax-percentage="0.5">
-                        <a href="https://www.youtube.com/watch?v=mFtwc6EQMFQ">
-                            <div class="work-thumb-wrapper">
-                                <div class="work-thumb-wrapper-cont">
-                                    <img src="/assets/img/thumbs/cdqc.png" alt="CDQC motion design entry">
-                                    <h3>CDQC</h3>
-                                    <p>Another motion design entry</p>
-    
-                                    <div class="cat-wrapper">
-                                        <div class="categories">
-                                            <span>&#9866; &nbsp</span>
-                                            <span>Motion design</span>
-                                        </div>
+    <HeaderFp />
+
+    <section id="who-am-i">
+        <Container class="fluid">
+            <GridX>
+                <div class="cell medium-8 large-5" id="wai-wrapper">
+                    <h2>Who am I?</h2>
+                    <q>Hello I’m Steven B-M Nguyen, an enthusiastic and cheerful motion, web and graphic designer from
+                        Melbourne, Australia.</q>
+
+                    <a href="/about-me" class="button">Read more</a>
+                </div>
+
+                <div class="cell medium-4 large-5"> 
+                    <!-- <VideoM></VideoM> -->
+                </div>
+
+            </GridX>
+        </Container>
+    </section>
+
+    <section id="featured-work">
+        <Container class="fluid extended">
+            <h2 class="rellax" data-rellax-speed="-0.8" data-rellax-percentage="1">Some of my work</h2>
+            <GridX class="grid-margin-x">
+                <div class="cell medium-6 large-6 work-thumb rellax" data-rellax-speed="-1" data-rellax-percentage="0.5">
+                    <a href="/portfolio/case/nihdc4">
+                        <div class="work-thumb-wrapper">
+                            <div class="work-thumb-wrapper-cont">
+                                <img src="/assets/img/thumbs/nihdc4.png" alt="NIHDC4 motion design entry">
+                                <h3>NIHDC4</h3>
+                                <p>Motion design entry</p>
+
+                                <div class="cat-wrapper">
+                                    <div class="categories">
+                                        <span>&#9866; &nbsp</span>
+                                        <span>Motion design</span>
                                     </div>
                                 </div>
-                                <div class="read-more-wrapper">
-                                    <div class="btn-read-more">See</div>
-                                </div>
+
                             </div>
-                        </a>
-    
-                    </div>
-    
-                    <div class="cell medium-6 large-4 work-thumb rellax" data-rellax-speed="-1" data-rellax-percentage="0.5">
-                        <a href="#null" class="disable-me">
-                            <div class="work-thumb-wrapper">
-                                <div class="work-thumb-wrapper-cont">
-                                    <img src="/assets/img/thumbs/maribsc-1.png" alt="s">
-                                    <h3>Maribyrnong College</h3>
-                                    <p>Stuff I did for my school that was never released.</p>
-                                </div>
+                            <div class="read-more-wrapper">
+                                <div class="btn-read-more">See</div>
                             </div>
-                        </a>
-                    </div>
-    
-                    <div class="cell medium-6 large-8 work-thumb rellax" data-rellax-speed="1" data-rellax-percentage="0.5">
-                        <a href="#null" class="disable-me">
-                            <div class="work-thumb-wrapper">
-                                <div class="work-thumb-wrapper-cont">
-                                    <img src="/assets/img/thumbs/homeroom-1.png" alt="s">
-                                    <h3>HomeRoom by Jikens & Jikano CO</h3>
-                                    <p>Online school enviroment redefined (School project)</p>
-    
-                                    <div class="cat-wrapper">
-                                        <div class="categories">
-                                            <span>&#9866; &nbsp</span>
-                                            <span>Graphic design, UI & UX, Industrial Design</span>
-                                        </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="cell medium-6 large-6 work-thumb rellax" data-rellax-speed="1" data-rellax-percentage="0.5">
+                    <a href="https://www.youtube.com/watch?v=mFtwc6EQMFQ">
+                        <div class="work-thumb-wrapper">
+                            <div class="work-thumb-wrapper-cont">
+                                <img src="/assets/img/thumbs/cdqc.png" alt="CDQC motion design entry">
+                                <h3>CDQC</h3>
+                                <p>Another motion design entry</p>
+
+                                <div class="cat-wrapper">
+                                    <div class="categories">
+                                        <span>&#9866; &nbsp</span>
+                                        <span>Motion design</span>
                                     </div>
                                 </div>
-                                <div class="read-more-wrapper">
-                                    <div class="btn-read-more">Read More</div>
+                            </div>
+                            <div class="read-more-wrapper">
+                                <div class="btn-read-more">See</div>
+                            </div>
+                        </div>
+                    </a>
+
+                </div>
+
+                <div class="cell medium-6 large-4 work-thumb rellax" data-rellax-speed="-1" data-rellax-percentage="0.5">
+                    <a href="#null" class="disable-me">
+                        <div class="work-thumb-wrapper">
+                            <div class="work-thumb-wrapper-cont">
+                                <img src="/assets/img/thumbs/maribsc-1.png" alt="s">
+                                <h3>Maribyrnong College</h3>
+                                <p>Stuff I did for my school that was never released.</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="cell medium-6 large-8 work-thumb rellax" data-rellax-speed="1" data-rellax-percentage="0.5">
+                    <a href="#null" class="disable-me">
+                        <div class="work-thumb-wrapper">
+                            <div class="work-thumb-wrapper-cont">
+                                <img src="/assets/img/thumbs/homeroom-1.png" alt="s">
+                                <h3>HomeRoom by Jikens & Jikano CO</h3>
+                                <p>Online school enviroment redefined (School project)</p>
+
+                                <div class="cat-wrapper">
+                                    <div class="categories">
+                                        <span>&#9866; &nbsp</span>
+                                        <span>Graphic design, UI & UX, Industrial Design</span>
+                                    </div>
                                 </div>
                             </div>
-                        </a>
-                    </div>
-    
-                    <WorkThumb 
-                        v-for="post in posts" 
-                        :key="post.title" 
-                        :title="post.title" 
-                        :slug="post.slug" 
-                        :img-src="post.img + '?' + post.id" 
-                        :desc="post.alt"  
-                        :cat="post.category" 
-                        :size="post.columns"
-                        class="rellax"
-                        :data-rellax-speed="rellaxPattern[posts.indexOf(post)]"
-                        data-rellax-percentage="0.5"
-                    />
-    
-    
-                    <p> {{data}}</p>
-    
-                    <div class="cell">
-                        <button class="button">See more!</button>
-                    </div>
-                </GridX>
-            </Container>
-    
-    
-        </section>
-    
-        <Cta />
+                            <div class="read-more-wrapper">
+                                <div class="btn-read-more">Read More</div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <WorkThumb 
+                    v-for="post in posts" 
+                    :key="post.title" 
+                    :title="post.title" 
+                    :slug="post.slug" 
+                    :img-src="post.img + '?' + post.id" 
+                    :desc="post.alt"  
+                    :cat="post.category" 
+                    :size="post.columns"
+                    class="rellax"
+                    :data-rellax-speed="rellaxPattern[posts.indexOf(post)]"
+                    data-rellax-percentage="0.5"
+                />
+
+
+                <p> {{data}}</p>
+
+                <div class="cell">
+                    <button class="button">See more!</button>
+                </div>
+            </GridX>
+        </Container>
+
+
+    </section>
+
+    <Cta />
 </template>
 
 <script>
@@ -143,6 +143,7 @@ export default {
 
     // this used to cause error 505, but may cause other issues
     beforeMount() {
+        document.querySelector("#__nuxt > div").id = "homepage"
     },
 
     mounted() {
@@ -162,7 +163,96 @@ export default {
         });
         
 
-    }
+        // loading in swave:
+        var swaveOne = bodymovin.loadAnimation({
+            container: document.getElementById('swave-1'),
+            path: 'assets/img/homepage/swave-1.json',
+            renderer: 'svg',
+            loop: false,
+            autoplay: true,
+        })
+        var swaveTwo = bodymovin.loadAnimation({
+            container: document.getElementById('swave-2'),
+            path: 'assets/img/homepage/swave-2.json',
+            renderer: 'svg',
+            loop: false,
+            autoplay: true,
+        })
+        // loading in swave:
+        var swaveOne = bodymovin.loadAnimation({
+            container: document.getElementById('swave-1-g'),
+            path: 'assets/img/homepage/swave-1.json',
+            renderer: 'svg',
+            loop: false,
+            autoplay: true,
+        })
+        var swaveTwo = bodymovin.loadAnimation({
+            container: document.getElementById('swave-2-g'),
+            path: 'assets/img/homepage/swave-2.json',
+            renderer: 'svg',
+            loop: false,
+            autoplay: true,
+        })
+
+        // when document ready execute swaves:
+        document.addEventListener("DOMContentLoaded", function (event) {
+            swaveOne.play();
+            swaveTwo.play();
+        });
+
+        // text logo header in
+        var textLogoHeader = anime.timeline({
+            easing: "spring(0.7, 100, 10, 4)",
+            duration: 400,
+            delay: anime.stagger(50),
+            loop: false,
+            autoplay: false,
+        });
+
+        textLogoHeader.add({
+            targets: '.header-letter',
+            opacity: 1,
+            translateY: [-viewportHeight , 0],  // make -1000 to the height of the window - done
+        })
+
+        let tah = 0
+
+        function animationToggleHeadText() {
+            if (tah == 0) {
+                textLogoHeader.play();
+                tah = 1;
+            }
+        };
+
+        let lastKnownScrollPosition = 0;
+        let ticking = false;
+
+        function doSomething(scrollPos) {
+            if (scrollPos >= document.getElementById("who-am-i").offsetTop) {
+                tah = 0;
+                //textLogoHeader.restart();
+            } else {
+                if (scrollPos > 100) {
+                    // console.log("scroll higher than 400");
+                    animationToggleHeadText();
+                }
+            }
+        }
+
+        document.addEventListener('scroll', function(e) {
+        lastKnownScrollPosition = window.scrollY;
+
+        if (!ticking) {
+            window.requestAnimationFrame(function() {
+            doSomething(lastKnownScrollPosition);
+            ticking = false;
+            });
+
+            ticking = true;
+        }
+        });
+        
+    }  
 }
 </script>
 
