4477aa565778ddae384c404637f3f412f5649ade
mechetle <23493254+mechetle@users.noreply.github.com>
2022-07-13 15:01:19 +1000

    renamed env prop

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 
 export default defineNuxtConfig({
   runtimeConfig: {
-    secretSauce: process.env.NUXT_SECRET_SAUCE,
+    secretSauce: process.env.SECRET_SAUCE,
   },
 
   css: [
