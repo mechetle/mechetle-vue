94944bd292fa07c98e1c711693562d5c1c8a2777
Steven B-M Nguyen <23493254+mechetle@users.noreply.github.com>
2024-06-17 22:44:03 +1000

    temporarily using old api

--- server/api/data.ts
+++ server/api/data.ts
@@ -10,86 +10,87 @@ the data.
 // older versions of node below v18 don't have fetch.
 //import fetch from "node-fetch";
 //import fetch from 'node-fetch-native'
-const api_url = `https://mache.mechetle.com`
+const api_url = `https://v1.mache.mechetle.com`;
 
-export default defineEventHandler (async (event) => {
-  let req = event.req
-  let res = event.res
+export default defineEventHandler(async (event) => {
+  let req = event.req;
+  let res = event.res;
 
-  let url_mod = api_url
+  let url_mod = api_url;
 
   interface Project {
-    name: string,
-    title: string,
-    finishDate: string,
-    category: string,
-    slug: string,
-    case: string,
-    body: string,
-    client: string,
-    client_field: string,
-    desc: string,
-    design_context: string,
-    alt: string,
-    columns: string,
-    img: string,
-    oneByOne: string,
-    video: string,
-    tags: string,
-    exported: string,
-    supplied: string,
-    callout: string,
+    name: string;
+    title: string;
+    finishDate: string;
+    category: string;
+    slug: string;
+    case: string;
+    body: string;
+    client: string;
+    client_field: string;
+    desc: string;
+    design_context: string;
+    alt: string;
+    columns: string;
+    img: string;
+    oneByOne: string;
+    video: string;
+    tags: string;
+    exported: string;
+    supplied: string;
+    callout: string;
   }
 
-  let data : Project[] = [];
+  let data: Project[] = [];
 
   //! IN THIS CURRENT EDGE RELEASE YOUT CAN DO /api/data/blah
-  
- /*  console.log("req.url:", req) */
+
+  /*  console.log("req.url:", req) */
   //req = req + "/refc"
   //const path = req.url.replace("/data", "");   // this is legacy according to node.js docs
-  
-  const req_url = new URL(`${api_url}/${req.url}`)
-  const searchParams = req_url.searchParams
 
-  let path = "/api"
+  const req_url = new URL(`${api_url}/${req.url}`);
+  const searchParams = req_url.searchParams;
+
+  let path = "/api";
 
   // implement this better
   if (searchParams.get("post")) {
-    path = `${path}/${searchParams.get("post")}`
+    path = `${path}/${searchParams.get("post")}`;
   }
   if (searchParams.get("limit") || searchParams.get("prev")) {
-    path = `${path}?${searchParams.toString()}`
+    path = `${path}?${searchParams.toString()}`;
   }
 
   console.log("URL path:", path); // https://nodejs.org/api/url.html
 
   try {
-    console.log("API key - starting with:", useRuntimeConfig().secretSauce.substring(0, 5))
+    console.log(
+      "API key - starting with:",
+      useRuntimeConfig().secretSauce.substring(0, 5)
+    );
   } catch (error) {
-    console.log("API KEY IS NOT ADDED")
-  }
-  
-  const headers = {
-    'secret-sauce': useRuntimeConfig().secretSauce
+    console.log("API KEY IS NOT ADDED");
   }
 
+  const headers = {
+    "secret-sauce": useRuntimeConfig().secretSauce,
+  };
+
   res.writeHead(200, { "Content-Type": "application/json" });
-  data = await $fetch(`${url_mod}${path}`, { method: 'GET', headers: headers});
-  
+  data = await $fetch(`${url_mod}${path}`, { method: "GET", headers: headers });
+
   /* fetch(`${url_mod}${path}`, { method: 'GET', headers: headers}).then((response) => response.json())
   .then((data_res: Project[]) => {
     data = data_res
     console.log(data)
   }); */
-  
-  console.log("Data:", data)
-  console.log("Length of data:", data.length)
-  
+
+  console.log("Data:", data);
+  console.log("Length of data:", data.length);
+
   res.write(JSON.stringify(data));
-  
+
   res.end();
   //console.log("headers:", res.getHeaders());
-
-
 });
